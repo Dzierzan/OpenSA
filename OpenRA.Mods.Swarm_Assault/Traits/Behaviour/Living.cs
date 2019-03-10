3164f5802265092d01acd9d4656367f7d59081cb
Paweł <dzierzan.12111994@gmail.com>
2019-03-10 18:46:10 +0100

    Added more stuff to the game
    Units now rotate when they stay idle. Added all four neutral bombers, currently they are heavily WIP. Added missing facings for colony turrets. Created missing facings for Spider Colony. Created custom icons for neutral bombers. Some minor changes. Added pirates as lobby option (not fully implemented).

--- OpenRA.Mods.Swarm_Assault/Traits/Behaviour/Living.cs
+++ OpenRA.Mods.Swarm_Assault/Traits/Behaviour/Living.cs
@@ -2,19 +2,13 @@ using OpenRA.Mods.Common.Traits;
 using OpenRA.Mods.Common.Traits.Render;
 using OpenRA.Traits;
 
-namespace OpenRA.Mods.Kknd.Traits.Behavior
+namespace OpenRA.Mods.Swarm_Assault.Traits.Behaviour
 {
 	[Desc("Makes infantry feel more alive by randomly rotating or playing an animation when idle.")]
 	class LivingInfo : ITraitInfo, Requires<MobileInfo>, Requires<WithSpriteBodyInfo>
 	{
 		[Desc("Chance per tick the actor rotates to a random direction.")]
 		public readonly int RotationChance = 1000;
-
-		[Desc("Chance per tick the actor triggers its bored sequence.")]
-		public readonly int BoredChance = 5000;
-
-		[Desc("Sequence to play when idle.")]
-		public readonly string BoredSequence = "bored";
 
 		public object Create(ActorInitializer init) { return new Living(init, this); }
 	}
@@ -38,9 +32,6 @@ namespace OpenRA.Mods.Kknd.Traits.Behavior
 			{
 				if (info.RotationChance > 0 && self.World.SharedRandom.Next(1, info.RotationChance) == 1)
 					mobile.Facing = self.World.SharedRandom.Next(0x00, 0xff);
-
-				if (info.BoredSequence != null && self.World.SharedRandom.Next(1, info.BoredChance) == 1)
-					wsb.PlayCustomAnimation(self, info.BoredSequence);
 			}
 		}
 	}
