d92735cc07a8d5992faaacbe08f76f6beb826a60
Paweł <dzierzan.12111994@gmail.com>
2020-04-25 17:55:55 +0200

    Added Graion's GrantPediodicCondition
    Changed spawning chance for seed and amount of projectiles.

---
+++ OpenRA.Mods.OpenSA/Traits/Conditions/GrantPeriodicCondition.cs
@@ -0,0 +1,166 @@
+using System;
+using OpenRA.Mods.Common.Traits;
+using OpenRA.Primitives;
+using OpenRA.Traits;
+
+namespace OpenRA.Mods.OpenSA.Traits
+{
+	[Desc("Grants a condition periodically.")]
+	public class GrantPeriodicConditionInfo : PausableConditionalTraitInfo
+	{
+		[GrantedConditionReference]
+		[FieldLoader.Require]
+		[Desc("The condition to grant.")]
+		public readonly string Condition = null;
+
+		[Desc("The range of time (in ticks) with the condition being disabled.")]
+		public readonly int[] CooldownDuration = { 1000 };
+
+		[Desc("The range of time (in ticks) with the condition being enabled.")]
+		public readonly int[] ActiveDuration = { 100 };
+
+		public readonly bool StartsGranted = false;
+
+		public readonly bool ShowSelectionBar = false;
+		public readonly Color CooldownColor = Color.DarkRed;
+		public readonly Color ActiveColor = Color.DarkMagenta;
+
+		public override object Create(ActorInitializer init) { return new GrantPeriodicCondition(init, this); }
+	}
+
+	public class GrantPeriodicCondition : PausableConditionalTrait<GrantPeriodicConditionInfo>, ISelectionBar, ITick, ISync
+	{
+		readonly Actor self;
+		readonly GrantPeriodicConditionInfo info;
+
+		ConditionManager manager;
+
+		[Sync]
+		int ticks;
+
+		int cooldown, active;
+		bool isSuspended;
+		int token = ConditionManager.InvalidConditionToken;
+
+		bool IsEnabled { get { return token != ConditionManager.InvalidConditionToken; } }
+
+		public GrantPeriodicCondition(ActorInitializer init, GrantPeriodicConditionInfo info)
+			: base(info)
+		{
+			self = init.Self;
+			this.info = info;
+		}
+
+		void SetDefaultState()
+		{
+			if (info.StartsGranted)
+			{
+				ticks = info.ActiveDuration.Length == 2
+					? self.World.SharedRandom.Next(info.ActiveDuration[0], info.ActiveDuration[1])
+					: info.ActiveDuration[0];
+				active = ticks;
+				if (info.StartsGranted != IsEnabled)
+					EnableCondition();
+			}
+			else
+			{
+				ticks = info.CooldownDuration.Length == 2
+					? self.World.SharedRandom.Next(info.CooldownDuration[0], info.CooldownDuration[1])
+					: info.CooldownDuration[0];
+				cooldown = ticks;
+				if (info.StartsGranted != IsEnabled)
+					DisableCondition();
+			}
+
+			isSuspended = false;
+		}
+
+		protected override void Created(Actor self)
+		{
+			manager = self.Trait<ConditionManager>();
+
+			if (!IsTraitDisabled)
+				SetDefaultState();
+
+			base.Created(self);
+		}
+
+		void ITick.Tick(Actor self)
+		{
+			if (!IsTraitDisabled && !IsTraitPaused && --ticks < 0)
+			{
+				if (IsEnabled)
+				{
+					ticks = info.CooldownDuration.Length == 2
+						? self.World.SharedRandom.Next(info.CooldownDuration[0], info.CooldownDuration[1])
+						: info.CooldownDuration[0];
+					cooldown = ticks;
+					DisableCondition();
+				}
+				else
+				{
+					ticks = info.ActiveDuration.Length == 2
+						? self.World.SharedRandom.Next(info.ActiveDuration[0], info.ActiveDuration[1])
+						: info.ActiveDuration[0];
+					active = ticks;
+					EnableCondition();
+				}
+			}
+		}
+
+		protected override void TraitEnabled(Actor self)
+		{
+			SetDefaultState();
+		}
+
+		protected override void TraitDisabled(Actor self)
+		{
+			if (IsEnabled)
+				DisableCondition();
+		}
+
+		protected override void TraitPaused(Actor self)
+		{
+			if (IsEnabled)
+			{
+				DisableCondition();
+				isSuspended = true;
+			}
+		}
+
+		protected override void TraitResumed(Actor self)
+		{
+			if (isSuspended)
+			{
+				EnableCondition();
+				isSuspended = false;
+			}
+		}
+
+		void EnableCondition()
+		{
+			if (token == ConditionManager.InvalidConditionToken)
+				token = manager.GrantCondition(self, info.Condition);
+		}
+
+		void DisableCondition()
+		{
+			if (token != ConditionManager.InvalidConditionToken)
+				token = manager.RevokeCondition(self, token);
+		}
+
+		float ISelectionBar.GetValue()
+		{
+			if (!info.ShowSelectionBar)
+				return 0f;
+
+			return IsEnabled
+				? (float)(active - ticks) / active
+					: (float)ticks / cooldown;
+		}
+
+		bool ISelectionBar.DisplayWhenEmpty { get { return info.ShowSelectionBar; } }
+
+		Color ISelectionBar.GetColor() { return IsEnabled ? info.ActiveColor : info.CooldownColor; }
+	}
+}
