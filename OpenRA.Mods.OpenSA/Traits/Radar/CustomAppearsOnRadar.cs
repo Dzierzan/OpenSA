3a445cb2a4ac7e8282ea30c0b18f48837c2ef11a
Andre Mohren <andremohren@gmail.com>
2020-08-05 23:00:21 +0200

    Updated radar behavior for actors.

---
+++ OpenRA.Mods.OpenSA/Traits/Radar/CustomAppearsOnRadar.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections.Generic;
+using OpenRA.Mods.Common.Traits;
+using OpenRA.Primitives;
+using OpenRA.Traits;
+
+namespace OpenRA.Mods.OpenSA.Traits.Radar
+{
+    public class CustomAppearsOnRadarInfo : TraitInfo
+    {
+        public readonly CVec Offset = CVec.Zero;
+
+        public override object Create(ActorInitializer init)
+        {
+            return new CustomAppearsOnRadar(this);
+        }
+    }
+
+    public class CustomAppearsOnRadar : IRadarSignature
+    {
+        private readonly CustomAppearsOnRadarInfo info;
+
+        public CustomAppearsOnRadar(CustomAppearsOnRadarInfo info)
+        {
+            this.info = info;
+        }
+
+        void IRadarSignature.PopulateRadarSignatureCells(Actor self, List<Pair<CPos, Color>> destinationBuffer)
+        {
+            var color = self.Owner.Color;
+
+            if (self.Owner.InternalName == "Neutral" || self.Owner.InternalName == "Creeps")
+                color = Color.FromArgb(227, 217, 205);
+
+            if (self.TraitOrDefault<Colony>() != null)
+            {
+                for (var y = 0; y < 6; y++)
+                for (var x = 0; x < 6; x++)
+                    destinationBuffer.Add(Pair.New(self.Location + new CVec(x, y) + info.Offset, color));
+            }
+            else
+                destinationBuffer.Add(Pair.New(self.Location, color));
+        }
+    }
+}
