5d5ac3aa40d50b22cc5902d865ebe9c0b6ac5cef
Paweł <dzierzan.12111994@gmail.com>
2020-10-30 19:51:10 +0100

    Added 058) War Front mission

---
+++ mods/sa/maps/058) War Front/script.lua
@@ -0,0 +1,15 @@
+Tick = function()
+
+end
+
+WorldLoaded = function()
+
+	Camera.Position = Actor245.CenterPosition
+
+	Ants = Player.GetPlayer("Ants")
+	Trigger.AfterDelay(DateTime.Seconds(6), function()
+		Ants.GrantCondition("enable-ants-ai")
+
+	end)
+
+end
