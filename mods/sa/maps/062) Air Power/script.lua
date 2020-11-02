ea35906e37c6c37024ed0f89429dc3a41dded89c
Paweł <dzierzan.12111994@gmail.com>
2020-11-02 20:47:00 +0100

    Added 062) Air Power mission

---
+++ mods/sa/maps/062) Air Power/script.lua
@@ -0,0 +1,17 @@
+Tick = function()
+
+end
+
+WorldLoaded = function()
+
+	Camera.Position = Actor215.CenterPosition
+
+	Ants = Player.GetPlayer("Ants")
+	Spiders = Player.GetPlayer("Spiders")
+	Trigger.AfterDelay(DateTime.Seconds(6), function()
+		Ants.GrantCondition("enable-ants-ai")
+		Spiders.GrantCondition("enable-spiders-ai")
+
+	end)
+
+end
