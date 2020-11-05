c7fbff273a5d08dac5c232d95d412e2b5f03062b
Paweł <dzierzan.12111994@gmail.com>
2020-11-05 23:40:26 +0100

    Added 070) X mission

---
+++ mods/sa/maps/070) X/script.lua
@@ -0,0 +1,19 @@
+Tick = function()
+
+end
+
+WorldLoaded = function()
+
+	Camera.Position = Actor217.CenterPosition
+
+	Ants = Player.GetPlayer("Ants")
+	Beetles = Player.GetPlayer("Beetles")
+	Wasps = Player.GetPlayer("Wasps")
+	Trigger.AfterDelay(DateTime.Seconds(6), function()
+		Ants.GrantCondition("enable-ants-ai")
+		Beetles.GrantCondition("enable-beetles-ai")
+		Wasps.GrantCondition("enable-wasps-ai")
+
+	end)
+
+end
