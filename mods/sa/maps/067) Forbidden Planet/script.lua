54aac27d46a2db5196ba9bac499f437aedaf7217
Paweł <dzierzan.12111994@gmail.com>
2020-11-05 22:51:44 +0100

    Added 067) Forbidden Planet mission

---
+++ mods/sa/maps/067) Forbidden Planet/script.lua
@@ -0,0 +1,15 @@
+Tick = function()
+
+end
+
+WorldLoaded = function()
+
+	Camera.Position = Actor312.CenterPosition
+
+	Spiders = Player.GetPlayer("Spiders")
+	Trigger.AfterDelay(DateTime.Seconds(6), function()
+		Spiders.GrantCondition("enable-spiders-ai")
+
+	end)
+
+end
