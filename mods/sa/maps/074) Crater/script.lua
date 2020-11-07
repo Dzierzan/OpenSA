c41fa353d40afab604de10814bd8f3e868c18aae
Paweł <dzierzan.12111994@gmail.com>
2020-11-07 18:03:23 +0100

    Added 074) Crater mission

---
+++ mods/sa/maps/074) Crater/script.lua
@@ -0,0 +1,17 @@
+Tick = function()
+
+end
+
+WorldLoaded = function()
+
+	Camera.Position = Actor223.CenterPosition
+
+	Beetles = Player.GetPlayer("Beetles")
+	Spiders = Player.GetPlayer("Spiders")
+	Trigger.AfterDelay(DateTime.Seconds(6), function()
+		Beetles.GrantCondition("enable-beetles-ai")
+		Spiders.GrantCondition("enable-spiders-ai")
+
+	end)
+
+end
