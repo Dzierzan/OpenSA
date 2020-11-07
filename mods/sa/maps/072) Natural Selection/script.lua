68a31cb2018435734fac487ebcef6bc30c0013fa
Dzierzan <dzierzan.12111994@gmail.com>
2020-11-07 11:49:01 +0100

    Added 072) Natural Selection mission

---
+++ mods/sa/maps/072) Natural Selection/script.lua
@@ -0,0 +1,17 @@
+Tick = function()
+
+end
+
+WorldLoaded = function()
+
+	Camera.Position = Actor161.CenterPosition
+
+	Ants = Player.GetPlayer("Ants")
+	Wasps = Player.GetPlayer("Wasps")
+	Trigger.AfterDelay(DateTime.Seconds(6), function()
+		Ants.GrantCondition("enable-ants-ai")
+		Wasps.GrantCondition("enable-wasps-ai")
+
+	end)
+
+end
