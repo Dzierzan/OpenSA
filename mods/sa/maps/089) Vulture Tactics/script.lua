7fdda4d4c6335ec2eeead3b94cf6b35274069169
Paweł <dzierzan.12111994@gmail.com>
2020-11-14 22:36:15 +0100

    Added 089) Vulture Tactics mission

---
+++ mods/sa/maps/089) Vulture Tactics/script.lua
@@ -0,0 +1,17 @@
+Tick = function()
+
+end
+
+WorldLoaded = function()
+
+	Camera.Position = Actor180.CenterPosition
+
+	Ants = Player.GetPlayer("Ants")
+	Beetles = Player.GetPlayer("Beetles")
+	Trigger.AfterDelay(DateTime.Seconds(6), function()
+		Ants.GrantCondition("enable-ants-ai")
+		Beetles.GrantCondition("enable-beetles-ai")
+
+	end)
+
+end
