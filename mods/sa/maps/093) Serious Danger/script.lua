09ca3151d961816e87cef7ab099e64eead63d84c
Paweł <dzierzan.12111994@gmail.com>
2020-11-15 00:48:59 +0100

    Added 093) Serious Danger mission

---
+++ mods/sa/maps/093) Serious Danger/script.lua
@@ -0,0 +1,15 @@
+Tick = function()
+
+end
+
+WorldLoaded = function()
+
+	Camera.Position = Actor304.CenterPosition
+
+	Ants = Player.GetPlayer("Ants")
+	Trigger.AfterDelay(DateTime.Seconds(6), function()
+		Ants.GrantCondition("enable-ants-ai")
+
+	end)
+
+end
