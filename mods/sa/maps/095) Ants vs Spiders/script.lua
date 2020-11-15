404f63a320a2da152c44116940a2f27d5d9e3c3f
Paweł <dzierzan.12111994@gmail.com>
2020-11-15 15:45:21 +0100

    Added 095) Ants vs Spiders mission

---
+++ mods/sa/maps/095) Ants vs Spiders/script.lua
@@ -0,0 +1,15 @@
+Tick = function()
+
+end
+
+WorldLoaded = function()
+
+	Camera.Position = Actor318.CenterPosition
+
+	Wasps = Player.GetPlayer("Wasps")
+	Trigger.AfterDelay(DateTime.Seconds(6), function()
+		Wasps.GrantCondition("enable-wasps-ai")
+
+	end)
+
+end
