e4b8594ac18becb04ba98b91f7b671ea2d2c8366
Matthias Mailänder <matthias@mailaender.name>
2021-02-09 20:05:17 +0100

    Fixup

--- mods/sa/maps/001_Colony_Defence/script.lua
+++ mods/sa/maps/001_Colony_Defence/script.lua
@@ -2,13 +2,13 @@ BotDelay =
 {
 	easy = 15,
 	normal = 6,
-	hard = 3,
+	hard = 3
 }
 
 WorldLoaded = function()
 	Camera.Position = Actor88.CenterPosition
 	Spiders = Player.GetPlayer("Spiders")
-	Trigger.AfterDelay(DateTime.Seconds(BotDelay[Difficulty]), function()
+	Trigger.AfterDelay(DateTime.Seconds(BotDelay[Map.LobbyOption("difficulty")]), function()
 		Spiders.GrantCondition("enable-spiders-ai")
 	end)
 end
