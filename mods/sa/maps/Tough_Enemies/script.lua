745e33da6520153609b724f8283c86e320506b09
Paweł <dzierzan.12111994@gmail.com>
2021-03-26 23:01:04 +0100

    Added custom mission Tough Enemies

---
+++ mods/sa/maps/Tough_Enemies/script.lua
@@ -0,0 +1,16 @@
+BotDelay =
+{
+	easy = 15,
+	normal = 6,
+	hard = 3
+}
+
+WorldLoaded = function()
+	Camera.Position = Actor260.CenterPosition
+	Wasps = Player.GetPlayer("Wasps")
+	Spiders = Player.GetPlayer("Spiders")
+	Trigger.AfterDelay(DateTime.Seconds(BotDelay[Map.LobbyOption("difficulty")]), function()
+		Wasps.GrantCondition("enable-wasps-ai")
+		Spiders.GrantCondition("enable-spiders-ai")
+	end)
+end
