0b8731ae05cc9bdc2ee634706df04287fc77fe64
Paweł <dzierzan.12111994@gmail.com>
2022-07-19 17:03:13 +0200

    Added Candy Siege custom mission by Hooet

---
+++ mods/sa/maps/Candy_Siege/script.lua
@@ -0,0 +1,18 @@
+BotDelay =
+{
+	easy = 15,
+	normal = 6,
+	hard = 3
+}
+
+WorldLoaded = function()
+	Camera.Position = Actor134.CenterPosition
+	Wasps = Player.GetPlayer("Wasps")
+	Ants = Player.GetPlayer("Ants")
+	Beetles = Player.GetPlayer("Beetles")
+	Trigger.AfterDelay(DateTime.Seconds(BotDelay[Map.LobbyOption("difficulty")]), function()
+		Wasps.GrantCondition("enable-wasps-ai")
+		Ants.GrantCondition("enable-ants-ai")
+		Beetles.GrantCondition("enable-beetles-ai")
+	end)
+end
