10dfd6ff8264455ad6834a9d0ea7594c7fa2457f
Paweł <dzierzan.12111994@gmail.com>
2022-07-19 21:17:03 +0200

    Added Puck-Man custom mission by Hooet

---
+++ mods/sa/maps/Puck-Man/script.lua
@@ -0,0 +1,18 @@
+BotDelay =
+{
+	easy = 15,
+	normal = 6,
+	hard = 3
+}
+
+WorldLoaded = function()
+	Camera.Position = Actor270.CenterPosition
+	Spiders = Player.GetPlayer("Spiders")
+	Ants = Player.GetPlayer("Ants")
+	Beetles = Player.GetPlayer("Beetles")
+	Trigger.AfterDelay(DateTime.Seconds(BotDelay[Map.LobbyOption("difficulty")]), function()
+		Spiders.GrantCondition("enable-spiders-ai")
+		Ants.GrantCondition("enable-ants-ai")
+		Beetles.GrantCondition("enable-beetles-ai")
+	end)
+end
