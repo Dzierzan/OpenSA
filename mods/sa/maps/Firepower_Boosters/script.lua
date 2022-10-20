285551b63dfade018135141379f3640e23ab64b7
Paweł <dzierzan.12111994@gmail.com>
2022-10-20 22:18:28 +0200

    Added Firepower Boosters mission

---
+++ mods/sa/maps/Firepower_Boosters/script.lua
@@ -0,0 +1,18 @@
+BotDelay =
+{
+	easy = 15,
+	normal = 6,
+	hard = 3
+}
+
+WorldLoaded = function()
+	Camera.Position = Actor391.CenterPosition
+	Ants = Player.GetPlayer("Ants")
+	Beetles = Player.GetPlayer("Beetles")
+	Wasps = Player.GetPlayer("Wasps")
+	Trigger.AfterDelay(DateTime.Seconds(BotDelay[Map.LobbyOption("difficulty")]), function()
+		Ants.GrantCondition("enable-ants-ai")
+		Beetles.GrantCondition("enable-beetles-ai")
+		Wasps.GrantCondition("enable-wasps-ai")
+	end)
+end
