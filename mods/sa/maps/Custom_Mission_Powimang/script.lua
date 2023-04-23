7041a748ae637012f940f52022018b373a3c8a46
Dzierzan <dzierzan.12111994@gmail.com>
2023-04-23 12:46:35 +0200

    Added Powimang mission by Hooet.

---
+++ mods/sa/maps/Custom_Mission_Powimang/script.lua
@@ -0,0 +1,21 @@
+BotDelay =
+{
+	easy = 12,
+	normal = 6,
+	hard = 3
+}
+
+WorldLoaded = function()
+	Camera.Position = Actor308.CenterPosition
+	Ants = Player.GetPlayer("Ants")
+	Beetles = Player.GetPlayer("Beetles")
+	Wasps = Player.GetPlayer("Wasps")
+	Spiders = Player.GetPlayer("Spiders")
+	Scorpions = Player.GetPlayer("Scorpions")
+	Trigger.AfterDelay(DateTime.Seconds(BotDelay[Map.LobbyOption("difficulty")]), function()
+		Ants.GrantCondition("enable-ants-ai")
+		Beetles.GrantCondition("enable-beetles-ai")
+		Wasps.GrantCondition("enable-wasps-ai")
+		Spiders.GrantCondition("enable-spiders-ai")
+	end)
+end
